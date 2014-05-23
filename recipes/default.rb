3bd7aa2dca7951c0f22bc2479c1b1a1acd9a786c
Eric Herot <eric.github@herot.com>
2014-05-23 15:35:54 -0400

    Make the linter happy

--- recipes/default.rb
+++ recipes/default.rb
@@ -51,7 +51,7 @@ file '/etc/network/interfaces' do
     "source /etc/network/interfaces.d/*\n"
   not_if do
     node['network_interfaces']['replace_orig'] ||
-    File.read('/etc/network/interfaces') =~ %r(^source /etc/network/interfaces.d/\*$)
+    File.read('/etc/network/interfaces') =~ %r{^source /etc/network/interfaces.d/\*$}
   end
   action :create
 end
